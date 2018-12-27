023f7fa8f8cffade072c3b8c3a5d64086520cc0f
Reece Bennett <reece.bennett1@gmail.com>
2018-12-27 11:08:13 +0000

    Update client app

--- public/main.js
+++ public/main.js
@@ -1,54 +1,143 @@
-function sel(query) { return document.querySelector(query); } 
+const APIURL = "http://localhost:3000/api";
+const data = [];
 
-const container = sel("#container");
-const grid = sel("#grid");
-const modalBg = sel("#modal-background");
-const modal = sel("#modal");
-const modalImg = sel("#modal > img");
-const modalUrl = sel("#modal > .url");
-const newButton = sel("#new-button");
+const container = document.getElementById("container");
+const grid = document.getElementById("grid");
 
-modalBg.addEventListener("click", closeModal);
+const lightbox = document.getElementById("lightbox");
+const lightboxLink = document.querySelector("#lightbox > a");
+const lightboxImage = document.querySelector("#lightbox > img");
+
+const form = document.querySelector("#newForm > form");
+const formImage = document.querySelector("#newForm img");
+const formImageUrl = document.querySelector("#imageURL");
+const formSourceUrl = document.querySelector("#sourceURL");
+
+// Get the data list from the API
+
+// TEMP - add random images to the data list
+function random(arr) {
+  let index = Math.floor(Math.random() * arr.length);
+  return arr[index];
+}
+const NUM = 20;
+for (let i = 0; i < NUM; i++) {
+  const w = 800;
+  const h = random([500, 600, 700]);
+  data.push({
+    source: "https://picsum.photos",
+    image: `https://picsum.photos/${w}/${h}/?random&t=${i}`
+  })
+}
+
+lightbox.addEventListener("click", (ev) => {
+  if (ev.target != lightbox) return;
+  closeLightbox();
+}, false);
+
+form.addEventListener("submit", (ev) => {
+  ev.preventDefault();
+  const data = {
+    imageURL: formImageUrl.value,
+    sourceURL: formSourceUrl.value
+  }
+  console.log(data);
+
+  // Send data to API
+  fetch(APIURL, {
+    method: "POST",
+    headers: { "Content-Type": "application/json" },
+    body: JSON.stringify(data)
+  })
+  .then(res => res.json())
+  .then(json => console.log(json));
+  // get new item in response, add into data and refresh page
+}, false);
+
+formImageUrl.addEventListener("focusout", (ev) => {
+  formImage.src = formImageUrl.value;
+}, false)
+
+for (let i = 0; i < data.length; i++) {
+  
+  const gridItem = document.createElement("div");
+  gridItem.classList.add("grid-item");
+  gridItem.addEventListener("click", () => openLightbox(i), false);
+  
+  const gridImg = document.createElement("img");
+  gridImg.src = data[i].image;
+  gridItem.append(gridImg);
+  
+  grid.append(gridItem);
+}
 
 const masonry = new Masonry(grid, {
   itemSelector: ".grid-item",
-  columnWidth: 200,
-  gutter: 10,
-  stagger: 30
+  columnWidth: 300,
+  fitWidth: true,
+  gutter: 5,
+  stagger: 20
 });
 
-function openModal(imageURL, sourceURL) {
-  if (!modalBg.hidden) return
-  
+imagesLoaded(grid).on("progress", function() {
+  masonry.layout();
+  console.log("progress");
+});
+
+let imageIndex = -1;
+
+function openLightbox(index) {
+  if (!lightbox.hidden) return
+
+  lightbox.hidden = false;
   container.classList.add("blur");
-  newButton.classList.add("blur");
 
-  modalBg.hidden = false;
-  modalImg.src = imageURL;
-  const domain = sourceURL.match(/^https?\:\/\/(?:www.?)([^\/:?#]+)(?:[\/:?#]|$)/i)[1];
-  modalUrl.innerText = domain;
-  modalUrl.href = sourceURL;
+  changeLightboxImage(index);
 }
 
-function closeModal() {
-  modalBg.hidden = true;
+function closeLightbox() {
+  if (lightbox.hidden) return
+  
+  lightbox.hidden = true;
   container.classList.remove("blur");
-  newButton.classList.remove("blur");
 }
 
-// Load the URLs and display images
-fetch("http://localhost:8080/api")
-  .then(res => {
-    return res.json();
-  })
-  .then(json => {
-    for (let i = 0; i < json.length; i++) {
-      const j = json[i];
-      const image = document.createElement("img");
-      image.src = j.imageURL;
-      image.classList.add("grid-item");
-      image.addEventListener("click", () => openModal(j.imageURL, j.sourceURL));
-      grid.append(image);
-      masonry.appended(image);
-    }
-  });
+function changeLightboxImage(index) {
+  const sourceURL = data[index].source;
+  const imageURL = data[index].image;
+
+  imageIndex = index;
+
+  lightboxLink.innerText = sourceURL;
+  lightboxLink.href = sourceURL;
+  
+  lightboxImage.src = imageURL;
+}
+
+function nextImage() {
+  if (lightbox.hidden) return;
+
+  imageIndex++;
+  changeLightboxImage(imageIndex);
+}
+
+function previousImage() {
+  if (lightbox.hidden) return;
+
+  imageIndex--;
+  changeLightboxImage(imageIndex);
+}
+
+document.addEventListener("keydown", (ev) => {
+  switch(ev.key) {
+    case "Escape":
+      closeLightbox();
+      break;
+    case "ArrowRight":
+      nextImage();
+      break;
+    case "ArrowLeft":
+      previousImage();
+      break;
+  }
+}, false);
