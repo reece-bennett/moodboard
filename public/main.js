7d5de22209356fcaba2eac742c4a3ce884d5fd14
Reece Bennett <reece.bennett1@gmail.com>
2018-12-27 11:33:53 +0000

    Get images from the API

--- public/main.js
+++ public/main.js
@@ -1,8 +1,16 @@
-const APIURL = "http://localhost:3000/api";
-const data = [];
+const APIURL = "http://localhost:8080/api";
+let data = [];
 
 const container = document.getElementById("container");
 const grid = document.getElementById("grid");
+
+const masonry = new Masonry(grid, {
+  itemSelector: ".grid-item",
+  columnWidth: 300,
+  fitWidth: true,
+  gutter: 5,
+  stagger: 20
+});
 
 const lightbox = document.getElementById("lightbox");
 const lightboxLink = document.querySelector("#lightbox > a");
@@ -12,23 +20,6 @@ const form = document.querySelector("#newForm > form");
 const formImage = document.querySelector("#newForm img");
 const formImageUrl = document.querySelector("#imageURL");
 const formSourceUrl = document.querySelector("#sourceURL");
-
-// Get the data list from the API
-
-// TEMP - add random images to the data list
-function random(arr) {
-  let index = Math.floor(Math.random() * arr.length);
-  return arr[index];
-}
-const NUM = 20;
-for (let i = 0; i < NUM; i++) {
-  const w = 800;
-  const h = random([500, 600, 700]);
-  data.push({
-    source: "https://picsum.photos",
-    image: `https://picsum.photos/${w}/${h}/?random&t=${i}`
-  })
-}
 
 lightbox.addEventListener("click", (ev) => {
   if (ev.target != lightbox) return;
@@ -58,27 +49,6 @@ formImageUrl.addEventListener("focusout", (ev) => {
   formImage.src = formImageUrl.value;
 }, false)
 
-for (let i = 0; i < data.length; i++) {
-  
-  const gridItem = document.createElement("div");
-  gridItem.classList.add("grid-item");
-  gridItem.addEventListener("click", () => openLightbox(i), false);
-  
-  const gridImg = document.createElement("img");
-  gridImg.src = data[i].image;
-  gridItem.append(gridImg);
-  
-  grid.append(gridItem);
-}
-
-const masonry = new Masonry(grid, {
-  itemSelector: ".grid-item",
-  columnWidth: 300,
-  fitWidth: true,
-  gutter: 5,
-  stagger: 20
-});
-
 imagesLoaded(grid).on("progress", function() {
   masonry.layout();
   console.log("progress");
@@ -103,8 +73,8 @@ function closeLightbox() {
 }
 
 function changeLightboxImage(index) {
-  const sourceURL = data[index].source;
-  const imageURL = data[index].image;
+  const sourceURL = data[index].sourceURL;
+  const imageURL = data[index].imageURL;
 
   imageIndex = index;
 
@@ -141,3 +111,21 @@ document.addEventListener("keydown", (ev) => {
       break;
   }
 }, false);
+
+// Get the data list from the API
+fetch(APIURL)
+.then(response => response.json())
+.then(json => {
+  data = json;
+  for (let i = 0; i < data.length; i++) {
+    const gridItem = document.createElement("div");
+    gridItem.classList.add("grid-item");
+    gridItem.addEventListener("click", () => openLightbox(i), false);
+    
+    const gridImg = document.createElement("img");
+    gridImg.src = data[i].imageURL;
+    gridItem.append(gridImg);
+    
+    grid.append(gridItem);
+  }
+});
