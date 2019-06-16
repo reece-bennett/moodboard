4645030fe2f6470330f79a4b7fe615dbfd4d91ee
Reece Bennett <reece.bennett1@gmail.com>
2019-06-16 15:05:17 +0100

    Implement login and server authentication

--- client/src/App.js
+++ client/src/App.js
@@ -1,20 +1,24 @@
 import React from "react";
-import logo from "./logo.svg";
+import { GoogleLogin, GoogleLogout } from "react-google-login";
 import "./App.css";
 
 export default class App extends React.Component {
   state = {
-    data: null
+    data: null,
+    secret: null,
+    user: null
   }
 
   componentDidMount() {
-    this.callBackend()
-    .then(res => this.setState({ data: res.message }))
-    .catch(err => console.error(err));
+    this.callBackend("/api")
+      .then(res => this.setState({ data: res.message }))
+      .catch(err => console.error(err));
   }
 
-  async callBackend() {
-    const response = await fetch("/api");
+  callBackend = async (url) => {
+    const { user } = this.state;
+    const headers = user ? { "Authorization": `Bearer ${user.idToken}` } : {};
+    const response = await fetch(url, { headers: headers });
     const body = await response.json();
 
     if (response.status !== 200) {
@@ -23,25 +27,53 @@ export default class App extends React.Component {
     return body;
   }
 
+  onSignIn = (googleUser) => {
+    const profile = googleUser.getBasicProfile();
+    const user = {
+      idToken: googleUser.tokenId,
+      id: profile.getId(),
+      givenName: profile.getGivenName(),
+      familyName: profile.getFamilyName(),
+      imageUrl: profile.getImageUrl()
+    };
+    this.setState({ user });
+    console.log(`${user.givenName} ${user.familyName} (ID:${user.id}) signed in`);
+    
+    this.callBackend("/secret")
+      .then(res => this.setState({ secret: res.message }))
+      .catch(err => console.error(err));
+  }
+
+  onSignOut = () => {
+    const { user } = this.state;
+    console.log(`${user.givenName} ${user.familyName} (ID:${user.id}) signed out`);
+    this.setState({ user: null });
+  }
+
   render() {
+    const signInButton = this.state.user ?
+      (
+        <GoogleLogout
+          onLogoutSuccess={this.onSignOut}
+          buttonText="Sign out"
+        />
+      ) : (
+        <GoogleLogin
+          clientId="551119125116-dh8arpi5njabjnqpamp10evsqk0ct87f.apps.googleusercontent.com"
+          onSuccess={this.onSignIn}
+          onFailure={(err) => console.error(`Sign in failed: ${err.error}`)}
+          buttonText="Sign in"
+          isSignedIn={true} // Try to auto sign in the user on page load
+        />
+      );
+
     return (
       <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-          <p>Data: {this.state.data}</p>
-        </header>
+        <h1>Moodboard</h1>
+        {signInButton}
+        <p>Data: {this.state.data}</p>
+        <p>Secret data: {this.state.secret}</p>
       </div>
-    );
+      );
   }
 }
--- server.js
+++ server.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
+const { OAuth2Client } = require("google-auth-library");
 
 const app = express();
 const port = process.env.PORT || 8080;
@@ -10,8 +11,51 @@ app.use(express.static(path.join(__dirname, "client", "build")));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 
+const CLIENT_ID = "551119125116-dh8arpi5njabjnqpamp10evsqk0ct87f.apps.googleusercontent.com";
+const authClient = new OAuth2Client(CLIENT_ID);
+
+app.use(async (req, res, next) => {
+  const authorization = req.header("Authorization");
+
+  if (authorization) {
+    try {
+      const idToken = authorization.split("Bearer ")[1];
+      const ticket = await authClient.verifyIdToken({
+        idToken: idToken,
+        audience: CLIENT_ID
+      });
+      const payload = ticket.getPayload();
+      req.user = payload.sub;
+      req.profile = {
+        email: payload.email,
+        name: payload.name,
+        givenName: payload.given_name,
+        familyName: payload.family_name,
+        picture: payload.picture
+      };
+    } catch (error) {
+      console.error("Failed to validate token");
+      console.error(error);
+    }
+  }
+
+  console.log(`${req.method} ${req.path} User:${req.user}`);
+  next();
+});
+
+function loggedIn(req, res, next) {
+  if (!req.user) {
+    return res.status(401).json({ message: "Unauthorized" });
+  }
+  next();
+}
+
 app.get("/api", (req, res) => {
   res.json({ message: "Hello world!" });
+});
+
+app.get("/secret", loggedIn, (req, res) => {
+  res.json({ message: `Secret page for ${req.user}` });
 });
 
 app.listen(port, () => console.log(`Listening on port ${port}`));
